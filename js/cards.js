361d52b505237747877ef28ddcb021d8448d8811
Arnaud Ardans II and Courtney Schild <arnaud@>
2019-09-25 19:30:10 -0700

    fixes javascript/jQuery bug

--- js/cards.js
+++ js/cards.js
@@ -1,14 +1,16 @@
-var suits = ["Diamonds","Hearts","Spades","Clubs"];
-var ranks = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"];
+$(document).ready(function() {
+  var suits = ["Diamonds","Hearts","Spades","Clubs"];
+  var ranks = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"];
 
-var deck = [];
+  var deck = [];
 
-suits.forEach(function(suit) {
-  ranks.forEach(function(rank) {
-  	deck.push(card + " of " + suit);
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push(rank + " of " + suit);
+    });
   });
-});
 
-deck.forEach(function(card){
-  $("ul").append("<li>" + card + "</li>");
+  deck.forEach(function(card) {
+    $("ul").append("<li>" + card + "</li>");
+  });
 });
